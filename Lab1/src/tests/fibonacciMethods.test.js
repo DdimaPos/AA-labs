9598dc7cdb74abdc6f91526391d3ff36f00fd1e8
DdimaPos <postoroncadumitru@gmail.com>
2025-02-19 22:14:54 +0200

    added additional loop to normalize the result

--- Lab1/src/tests/fibonacciMethods.test.js
+++ Lab1/src/tests/fibonacciMethods.test.js
@@ -2,47 +2,50 @@ import { fibonacciFastDoubling } from "../fastDoubling";
 import { fibonacciIterative } from "../inPlaceMethod";
 
 describe("performance test of algorithms", () => {
+  var testedN = [0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000];
+  var nOfTests = 100;
+
   test("performance of fibonacci fast doubling", () => {
     var results = [];
-    var testedN = [0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000];
     testedN.forEach((el) => {
       var start = performance.now();
 
-      fibonacciFastDoubling(el);
+      for (let i = 0; i < nOfTests; i++) {
+        fibonacciFastDoubling(el);
+      }
 
       var end = performance.now();
 
-      results.push(end - start);
+      results.push((end - start) / nOfTests);
     });
     console.table(results);
   });
 
   test("performance of fibonacci iterative method", () => {
     var results = [];
-    var testedN = [0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000];
     testedN.forEach((el) => {
       var start = performance.now();
 
-      fibonacciIterative(el);
-
+      for (let i = 0; i < nOfTests; i++) {
+        fibonacciIterative(el);
+      }
       var end = performance.now();
 
-      results.push(end - start);
+      results.push((end - start) / nOfTests);
     });
     console.table(results);
   });
 
   test("performance of fibonacci method with memoization", () => {
     var results = [];
-    var testedN = [0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000];
     testedN.forEach((el) => {
       var start = performance.now();
-
-      fibonacciIterative(el);
-
+      for (let i = 0; i < nOfTests; i++) {
+        fibonacciIterative(el);
+      }
       var end = performance.now();
 
-      results.push(end - start);
+      results.push((end - start) / nOfTests);
     });
     console.table(results);
   });
