5666c261d946663f7b1bc3870a5627e5874e042c
DdimaPos <postoroncadumitru@gmail.com>
2025-03-03 00:27:07 +0200

    feat: added switch to choose the sort method

--- Lab2/code/src/App.tsx
+++ Lab2/code/src/App.tsx
@@ -3,15 +3,33 @@ import './App.css'
 import MergeSortVisualizer from './sorts/mergesort'
 import HeapSortVisualizer from './sorts/heapsort'
 import QuickSortVisualizer from './sorts/quicksort'
-
+import { Button } from './components/ui/button'
+type Variants = 0 | 1 | 2| 3;  
 function App() {
+  const [variant, setVariant] = useState<Variants>(0)
+  const renderVisualizer = () =>{
 
+        switch(variant){
+          case 0:
+            return <MergeSortVisualizer/>
+          case 1:
+            return <HeapSortVisualizer initialArray={[1,23,4,5,45,2,43,73,11]}/>
+          case 2:
+            return <QuickSortVisualizer/>
+          case 3:
+            return <MergeSortVisualizer/>
+        }
+  }
   return (
-    <>
-      {/*<MergeSortVisualizer/>*/}
-      {/*<HeapSortVisualizer initialArray={[1,23,4,5,45,2,43,73,11]}/>*/}
-      <QuickSortVisualizer/>
-    </>
+    <div>
+      <div className='flex gap-2 justify-center'>
+        <Button onClick={()=> setVariant(0)}>Merge Sort</Button>
+        <Button onClick={()=> setVariant(1)}>Heap Sort</Button>
+        <Button onClick={()=> setVariant(2)}>Quick Sort</Button>
+        <Button onClick={()=> setVariant(3)}>Placeholder sort</Button>
+      </div>
+      {renderVisualizer()}
+    </div>
   )
 }
 
